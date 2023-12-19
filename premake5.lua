7d6da9e1286a4e05b702a1a6f7fcde3d5da67880
Aymeric Robert <aymerob@gmail.com>
2023-12-19 18:53:36 +0100

    GLFW submodule fix

--- premake5.lua
+++ premake5.lua
@@ -14,9 +14,120 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 IncludeDir = {}
 IncludeDir["GLFW"] = "Ava/submodules/GLFW/include"
 
-include "Ava/submodules/GLFW"
+project "GLFW"
+	location "Ava/submodules/GLFW"
+	kind "StaticLib"
+	language "C"
+	staticruntime "off"
+	warnings "off"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"Ava/submodules/GLFW/include/GLFW/glfw3.h",
+		"Ava/submodules/GLFW/include/GLFW/glfw3native.h",
+		"Ava/submodules/GLFW/src/glfw_config.h",
+		"Ava/submodules/GLFW/src/context.c",
+		"Ava/submodules/GLFW/src/init.c",
+		"Ava/submodules/GLFW/src/input.c",
+		"Ava/submodules/GLFW/src/monitor.c",
 
+		"Ava/submodules/GLFW/src/null_init.c",
+		"Ava/submodules/GLFW/src/null_joystick.c",
+		"Ava/submodules/GLFW/src/null_monitor.c",
+		"Ava/submodules/GLFW/src/null_window.c",
 
+		"Ava/submodules/GLFW/src/platform.c",
+		"Ava/submodules/GLFW/src/vulkan.c",
+		"Ava/submodules/GLFW/src/window.c",
+	}
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		
+		files
+		{
+			"Ava/submodules/GLFW/src/x11_init.c",
+			"Ava/submodules/GLFW/src/x11_monitor.c",
+			"Ava/submodules/GLFW/src/x11_window.c",
+			"Ava/submodules/GLFW/src/xkb_unicode.c",
+			"Ava/submodules/GLFW/src/posix_module.c",
+			"Ava/submodules/GLFW/src/posix_time.c",
+			"Ava/submodules/GLFW/src/posix_thread.c",
+			"Ava/submodules/GLFW/src/posix_module.c",
+			"Ava/submodules/GLFW/src/glx_context.c",
+			"Ava/submodules/GLFW/src/egl_context.c",
+			"Ava/submodules/GLFW/src/osmesa_context.c",
+			"Ava/submodules/GLFW/src/linux_joystick.c"
+		}
+
+		defines
+		{
+			"_GLFW_X11"
+		}
+
+	filter "system:macosx"
+		pic "On"
+
+		files
+		{
+			"Ava/submodules/GLFW/src/cocoa_init.m",
+			"Ava/submodules/GLFW/src/cocoa_monitor.m",
+			"Ava/submodules/GLFW/src/cocoa_window.m",
+			"Ava/submodules/GLFW/src/cocoa_joystick.m",
+			"Ava/submodules/GLFW/src/cocoa_time.c",
+			"Ava/submodules/GLFW/src/nsgl_context.m",
+			"Ava/submodules/GLFW/src/posix_thread.c",
+			"Ava/submodules/GLFW/src/posix_module.c",
+			"Ava/submodules/GLFW/src/osmesa_context.c",
+			"Ava/submodules/GLFW/src/egl_context.c"
+		}
+
+		defines
+		{
+			"_GLFW_COCOA"
+		}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		files
+		{
+			"Ava/submodules/GLFW/src/win32_init.c",
+			"Ava/submodules/GLFW/src/win32_joystick.c",
+			"Ava/submodules/GLFW/src/win32_module.c",
+			"Ava/submodules/GLFW/src/win32_monitor.c",
+			"Ava/submodules/GLFW/src/win32_time.c",
+			"Ava/submodules/GLFW/src/win32_thread.c",
+			"Ava/submodules/GLFW/src/win32_window.c",
+			"Ava/submodules/GLFW/src/wgl_context.c",
+			"Ava/submodules/GLFW/src/egl_context.c",
+			"Ava/submodules/GLFW/src/osmesa_context.c"
+		}
+			defines 
+		{ 
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+		filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter { "system:windows", "configurations:Debug-AS" }	
+		runtime "Debug"
+		symbols "on"
+		sanitize { "Address" }
+		flags { "NoRuntimeChecks", "NoIncrementalLink" }
+		filter "configurations:Release"
+		runtime "Release"
+		optimize "speed"
+		filter "configurations:Dist"
+		runtime "Release"
+		optimize "speed"
+		symbols "off"
 
 project "Ava"
 	location "Ava"
@@ -128,3 +239,5 @@ project "SandBox"
 	filter "configurations:distribution"
 		defines "AVA_DISTRIBUTION"
 		optimize "On"
+
+
